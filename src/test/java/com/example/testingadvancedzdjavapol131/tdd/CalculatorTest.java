0553accc40a1bc5f28b5c39e3449261a9797b0c6
Pjoter K <piotrz9522@gmail.com>
2023-05-14 11:52:04 +0200

    Ass AssertJ assertions

--- src/test/java/com/example/testingadvancedzdjavapol131/tdd/CalculatorTest.java
+++ src/test/java/com/example/testingadvancedzdjavapol131/tdd/CalculatorTest.java
@@ -1,21 +1,35 @@
 package com.example.testingadvancedzdjavapol131.tdd;
 
-import org.junit.jupiter.api.Assertions;
+import org.assertj.core.api.Assertions;
+import org.assertj.core.data.Percentage;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class CalculatorTest {
 
+    Calculator calculator;
+
+    @BeforeEach
+    void initCalculator(){
+        calculator = new Calculator();
+    }
     @Test
     void whenAddTwoNumbersThenReturnSumOfTheNumbers(){
         //given
-        Calculator calculator = new Calculator();
-
+//        Calculator calculator = new Calculator();
+//        Calculator initiated in @BeforeEach
         //when
         int result = calculator.add(2,3);
 
         //then
-        Assertions.assertEquals(5, result);
-        Assertions.assertNotEquals(4, result);
-
+        assertEquals(5, result);
+        assertNotEquals(4, result);
+        Assertions.assertThat(result).isPositive();
+        Assertions.assertThat(result).isGreaterThan(2);
+        Assertions.assertThat(result).isNotZero();
+        Assertions.assertThat(result).isCloseTo(4, Percentage.withPercentage(90));
     }
 }
