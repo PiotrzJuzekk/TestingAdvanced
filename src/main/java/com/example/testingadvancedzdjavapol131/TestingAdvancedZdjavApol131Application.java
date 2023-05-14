5fc2870aa256f37cb72363dfc6f00cd449987585
Pjoter K <piotrz9522@gmail.com>
2023-05-14 10:39:40 +0200

    New project create

---
+++ src/main/java/com/example/testingadvancedzdjavapol131/TestingAdvancedZdjavApol131Application.java
@@ -0,0 +1,13 @@
+package com.example.testingadvancedzdjavapol131;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingAdvancedZdjavApol131Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestingAdvancedZdjavApol131Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testingadvancedzdjavapol131/TestingAdvancedZdjavApol131ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testingadvancedzdjavapol131;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingAdvancedZdjavApol131ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
